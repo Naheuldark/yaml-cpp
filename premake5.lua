99aa384b2211a0425a4bf3882f0ceb58f2e28b93
Julien Bordas <julien.bordas@amadeus.com>
2021-05-05 17:38:40 +0200

    Modify premake

--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,29 @@
 project "yaml-cpp"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
+	files {
 		"src/**.h",
 		"src/**.cpp",
 		
 		"include/**.h"
 	}
 
-	includedirs
-	{
+	includedirs {
 		"include"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
